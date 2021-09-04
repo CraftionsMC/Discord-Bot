6b73bb4ab0824b1f1f6c31e10805a7e2ef148556
MCTzOCK <dev@mctzock.de>
2021-09-04 12:57:49 +0200

    Added Ban and Kick Command

---
+++ src/commands/ban.js
@@ -0,0 +1,43 @@
+/**
+ * @author Ben Siebert <ben@mctzock.de>
+ * @copyright (c) 2018-2021 Ben Siebert. All rights reserved.
+ */
+
+const {MessageEmbed} = require("discord.js");
+module.exports = {
+    execute: (msg) => {
+        if (msg.mentions.members.first()) {
+            msg.mentions.members.first().ban();
+            msg.reply({
+                embeds: [
+                    new MessageEmbed({
+                        title: "Erfolgreich",
+                        color: "#10e83b",
+                        description: `Der Nutzer wurde erfolgreich gebannt!`,
+                        author: {
+                            name: "0erPinq Bot",
+                            icon_url: "https://avatars.githubusercontent.com/u/90091315?s=200&v=4"
+                        },
+                        timestamp: new Date()
+                    })
+                ]
+            })
+        } else {
+            msg.reply({
+                embeds: [
+                    new MessageEmbed({
+                        title: "Fehler",
+                        color: "#e81010",
+                        description: `Du musst den Nutzer den du bannen willst pingen!`,
+                        author: {
+                            name: "0erPinq Bot",
+                            icon_url: "https://avatars.githubusercontent.com/u/90091315?s=200&v=4"
+                        },
+                        timestamp: new Date()
+                    })
+                ]
+            })
+        }
+    },
+    admin: true
+}
